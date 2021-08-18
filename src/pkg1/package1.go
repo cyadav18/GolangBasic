0ad1167f4c6c4724802f602cbfc3daa37f208cd8
cyadav18 <chethanyadav5@gmail.com>
2021-08-18 22:30:19 +0530

    refactoring

--- src/pkg1/package1.go
+++
@@ -1,9 +0,0 @@
-package pkg1
-
-var Pack1Int int = 42
-var Pack1Float = 3.14
-
-//returning from pkg1
-func ReturnStr() string {
-	return "Hello main! pkg1"
-}
--- src/pkg2/package2.go
+++
@@ -1,9 +0,0 @@
-package pkg2
-
-var Pack1Int int = 44
-var Pack1Float = 3.1432434
-
-//returning from pkg2
-func ReturnStr() string {
-	return "Hello main! pkg2"
-}
